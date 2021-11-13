58f568554d9b46b953a6ba8559fc277c5ff57df6
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-11-14 00:26:28 +0200

    #1 Helpers: edited filter function for helper1

--- 1-lesson-homework/helpers/helper1.js
+++ 1-lesson-homework/helpers/helper1.js
@@ -1,5 +1,34 @@
+const testData = require('../../data-from-test-work.json');
+
+const jsonString = JSON.stringify(testData);
+
+const parsedData = JSON.parse(jsonString);
+
+
+let filtredArray = [];
+
+
 const filterKeyAndValue = (array, key, value ) => {
-  let filtredArray = [];
+
+  filtredArray = [];
+
+  parsedData.test.forEach(object => {
+    if (object[key] === key) {
+    filtredArray.push(object);
+    } else if (object[key] === value) {
+      filtredArray.push(object);
+    }
+
+
+  });
+
+  return filtredArray;
+
 };
+
+
+console.log(filterKeyAndValue(parsedData, 'item', 'orange'));
+
+console.log(filterKeyAndValue(parsedData, 'weight', 4));
 
 module.exports = filterKeyAndValue();
