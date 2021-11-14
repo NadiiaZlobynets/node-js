dac7b1f2b00196b253838223ca3f2c6c18072ed0
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-11-14 17:43:30 +0200

    #1 Helpers: connected all helpers in index.js

---
+++ 1-lesson-homework/helpers/index.js
@@ -0,0 +1,17 @@
+const helper1 = require('./helper1');
+
+const helper2 = require('./helper2');
+
+const helper3 = require('./helper3');
+
+  console.log(helper1);
+
+  console.log(helper2);
+
+  console.log(helper3);
+
+module.exports = {
+  helper1,
+  helper2,
+  helper3
+};
