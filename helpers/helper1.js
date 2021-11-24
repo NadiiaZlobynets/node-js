842a458e4d9b113d375f2eafdfd2365832396741
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-11-24 13:30:16 +0200

    1# Helpers: changed function helper1

--- helpers/helper1.js
+++ helpers/helper1.js
@@ -7,12 +7,22 @@ const helper1 = (array, key, value ) => {
   filtredArray = [];
 
   testData.test.forEach(object => {
-    if (object[key] === key) {
-    filtredArray.push(object);
-    } else if (object[key] === value) {
+
+    if (key === 'weight' && value === object.weight) {
+      filtredArray.push(object);
+    } else if (key === 'item' && value === object.item) {
+      filtredArray.push(object);
+    } else if (key === 'type' && value === object.item) {
+      filtredArray.push(object);
+    } else if (key === 'quantity' && value === object.item) {
+      filtredArray.push(object);
+    } else if (key === 'pricePeritem' && value === object.item) {
+      filtredArray.push(object);
+    } else if (key === 'pricePerKilo' && value === object.item) {
       filtredArray.push(object);
     }
-  });
+
+    });
 
   console.log(filtredArray);
 
@@ -20,8 +30,12 @@ const helper1 = (array, key, value ) => {
 
 };
 
-// console.log(helper1(parsedData, 'item', 'orange'));
+helper1(testData, 'item', 'orange');
 
-// console.log(helper1(parsedData, 'weight', 4));
+// console.log(helper1(testData, 'item', 'orange'));
+
+// helper1(testData, 'weight', 4);
+
+// console.log(helper1(testData, 'weight', 4));
 
 module.exports = helper1();
