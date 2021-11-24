bbf3addc0975b1896ca45484370c794c3db79d65
Nadiia.Zlobynets <euphoria93@gmail.com>
2021-11-24 14:13:54 +0200

    1# Helpers: changed function helper3: used map instead of forEach

--- helpers/helper3.js
+++ helpers/helper3.js
@@ -1,40 +1,71 @@
 const testData = require('../dataFromTestWork.json');
 
-let calculatedPrice = [];
-
-const helper3 = (array, key, value ) => {
-
-  calculatedPrice = [];
-
-  testData.test.forEach(object => {
-
-    if(object.item === value || object.type === value) {
-
-    if (object.pricePerKilo !== undefined) {
-      object.pricePerKilo =
-      object.pricePerKilo.replace('$', '').split(',').join('.');
-      price = object.pricePerKilo;
-      price *= object.weight;
-      object.price = price;
-      calculatedPrice.push(object);
-
-    } else if (object.pricePeritem !== undefined) {
-      object.pricePeritem =
-      object.pricePeritem.replace('$', '').split(',').join('.');
-      price = object.pricePeritem;
-      price *= object.quantity;
-      object.price = price;
-      calculatedPrice.push(object);
-    }
-  }
+const helper3 = testData.test.map(object => {
+if(object.pricePerKilo !== undefined) {
+  object.price = parseFloat(object.pricePerKilo.replace('$', ''))*object.weight
+    return object;
+}
+if (object.pricePeritem !== undefined) {
+  object.price = parseFloat(object.pricePeritem.replace('$', ''))*object.quantity
+  return object;
+}
   });
 
-  console.log(calculatedPrice);
+console.log(helper3);
 
-  return calculatedPrice;
+module.exports = helper3;
 
-};
 
-helper3(testData.test, 'item', 'orange');
 
-module.exports = helper3();
+
+
+
+
+
+
+
+
+
+
+
+
+// const testData = require('../dataFromTestWork.json');
+
+// let calculatedPrice = [];
+
+// const helper3 = (array, key, value ) => {
+
+//   calculatedPrice = [];
+
+//   testData.test.forEach(object => {
+
+//     if(object.item === value || object.type === value) {
+
+//     if (object.pricePerKilo !== undefined) {
+//       object.pricePerKilo =
+//       object.pricePerKilo.replace('$', '').split(',').join('.');
+//       price = object.pricePerKilo;
+//       price *= object.weight;
+//       object.price = price;
+//       calculatedPrice.push(object);
+
+//     } else if (object.pricePeritem !== undefined) {
+//       object.pricePeritem =
+//       object.pricePeritem.replace('$', '').split(',').join('.');
+//       price = object.pricePeritem;
+//       price *= object.quantity;
+//       object.price = price;
+//       calculatedPrice.push(object);
+//     }
+//   }
+//   });
+
+//   console.log(calculatedPrice);
+
+//   return calculatedPrice;
+
+// };
+
+// helper3(testData.test, 'item', 'orange');
+
+// module.exports = helper3();
